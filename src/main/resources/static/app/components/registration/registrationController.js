5ac9ba7f18f88599eccc7b3e81e9a70f015f2ca0
wkennedy <wildo.waggins@gmail.com>
2015-12-06 14:38:20 -0500

    Added confirmation modal

--- src/main/resources/static/app/components/registration/registrationController.js
+++ src/main/resources/static/app/components/registration/registrationController.js
@@ -1,19 +1,25 @@
 var registrationControllers = angular.module('registrationControllers', []);
 
 registrationControllers.controller('RegistrationCtrl', ['$scope', '$q', 'registrationService',
-    function($scope, $q, registrationService) {
+    function ($scope, $q, registrationService) {
 
         $scope.registration = {};
 
-        $scope.saveRegistration = function(registration) {
+        $scope.modalShown = false;
+        $scope.toggleModal = function () {
+            $scope.modalShown = !$scope.modalShown;
+        };
+
+        $scope.saveRegistration = function (registration) {
             $scope.errors = {};
             console.log($scope.registration);
-            registrationService.save($scope.registration).then(function(success) {
+            registrationService.save($scope.registration).then(function (success) {
                 console.debug("Registration Saved")
-            },function(error) {
-                console.log("error in controller");
-                console.log(error);
+            }, function (error) {
+                //console.log(error);
                 $scope.errors.api = error.data.errors[0].code;
             });
-        }
+            $scope.modalShown = false;
+        };
+
     }]);
